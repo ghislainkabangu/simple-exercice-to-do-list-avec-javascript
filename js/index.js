902c2e93a00ec34a4bcf6a043af5ad55863af971
ghislain kabangu <142664005+ghislainkabangu@users.noreply.github.com>
2024-03-04 11:33:05 +0100

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -2,25 +2,34 @@
 
 let form = document.querySelector("form");
 let texte;
-
 let printData = () => {
   let body = document.getElementsByTagName("body")[0];
   let bloc = document.createElement("div");
   bloc.title =
-    "vous pouvez le retirer de la liste en cliquant sur bouton droit";
+    "vous pouvez retirer cette ligne de la liste en cliquant sur bouton droit";
   bloc.classList.add("divbloc");
+
+  /****on attribue une classe ("divbloc") à tous les Divs pour faciliter le comptage dans le tableau**** */
   let blocs = document.querySelectorAll(".divbloc").length + 1;
+  let numOrdre = document.getElementById("numOrdre");
+  numOrdre.innerHTML = `nombre d'élément créer: ${blocs}`;
 
+  /****************************************************** */
   let textEcrit = document.createElement("p");
-  textEcrit.innerHTML = blocs + ")" + " " + texte;
+  textEcrit.innerHTML = texte;
 
+  /**********************creation du bouton de suppression **************************/
   let sup = document.createElement("button");
-  sup.title = "retirer de la liste";
+  sup.title = "Cliquez ici pour retirer cette ligne de la liste";
   sup.innerHTML = "retirer de la liste";
-
+  /*************************evenement lors de la suppression********************** */
   sup.addEventListener("click", () => {
     bloc.parentNode.removeChild(bloc);
+
+    blocs = document.querySelectorAll(".divbloc").length;
+    numOrdre.innerHTML = `nombre d'élément créer: ${blocs}`;
   });
+  /*********************************** *******************/
 
   body.appendChild(bloc);
   bloc.appendChild(textEcrit);
