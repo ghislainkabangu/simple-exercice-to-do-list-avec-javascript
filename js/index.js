bba641a981fca14be519eebb775f26a581db2179
ghislain kabangu <142664005+ghislainkabangu@users.noreply.github.com>
2024-03-19 02:21:14 +0100

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -2,39 +2,50 @@
 
 let form = document.querySelector("form");
 let texte;
+
 let printData = () => {
-  let body = document.getElementsByTagName("body")[0];
+  let section = document.getElementsByTagName("section")[0];
   let bloc = document.createElement("div");
-  bloc.title =
-    "vous pouvez retirer cette ligne de la liste en cliquant sur bouton droit";
   bloc.classList.add("divbloc");
 
   /****on attribue une classe ("divbloc") à tous les Divs pour faciliter le comptage dans le tableau**** */
   let blocs = document.querySelectorAll(".divbloc").length + 1;
   let numOrdre = document.getElementById("numOrdre");
-  numOrdre.innerHTML = `nombre d'élément créer: ${blocs}`;
+  numOrdre.innerHTML = `nombre de tâche: ${blocs}`;
 
   /****************************************************** */
   let textEcrit = document.createElement("p");
   textEcrit.innerHTML = texte;
-
+  /*************** modification*********************************** */
+  let modifier = document.createElement("button");
+  modifier.innerHTML = "modifier";
+  modifier.title = "cliquer pour modifier";
+  /******************event lors de la supression */
+  modifier.addEventListener("click", () => {
+    let textOrgine = bloc.firstChild.innerHTML;
+    let newText = prompt("modifier le texte ", textOrgine);
+    if (newText != "") {
+      textEcrit.innerHTML = newText;
+    }
+  });
   /**********************creation du bouton de suppression **************************/
   let sup = document.createElement("button");
-  sup.title = "Cliquez ici pour retirer cette ligne de la liste";
-  sup.innerHTML = "retirer de la liste";
+  sup.title = "Cliquez pour suprimer";
+  sup.innerHTML = "suprimer";
   /*************************evenement lors de la suppression********************** */
   sup.addEventListener("click", () => {
     bloc.parentNode.removeChild(bloc);
 
     blocs = document.querySelectorAll(".divbloc").length;
-    numOrdre.innerHTML = `nombre d'élément créer: ${blocs}`;
+    numOrdre.innerHTML = `nombre de tâche: ${blocs}`;
   });
+
   /*********************************** *******************/
 
-  body.appendChild(bloc);
+  section.appendChild(bloc);
   bloc.appendChild(textEcrit);
+  bloc.appendChild(modifier);
   bloc.appendChild(sup);
-  console.log(blocs);
 };
 
 form.addEventListener("submit", (e) => {
